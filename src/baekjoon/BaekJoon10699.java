78c20229ec0651e6fb6e2ab96049c3790a84f8ca
Tinamiru <sjf95@naver.com>
2022-05-15 11:47:02 +0900

    BaekJoon Algorithm

---
+++ src/baekjoon/BaekJoon10699.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.time.LocalDate;
+
+public class BaekJoon10699 {
+	public static void main(String[] args) {
+
+		LocalDate d = LocalDate.now();
+		System.out.println(d);
+
+	}
+}
