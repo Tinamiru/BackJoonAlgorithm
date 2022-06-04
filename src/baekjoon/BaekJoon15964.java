f2af26b063470f96f6f54d9d8d9971b801bb55c7
Tinamiru <sjf95@naver.com>
2022-06-04 22:47:23 +0900

    i have a cold

---
+++ src/baekjoon/BaekJoon15964.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BaekJoon15964 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+
+        System.out.println((a + b) * (a - b));
+
+
+    }
+}
+
