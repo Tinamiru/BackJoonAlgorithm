324943bdd30ad5098ba73de9581deb350a830f87
Tinamiru <sjf95@naver.com>
2022-06-05 11:47:02 +0900

    BaekJoon Algorithm

---
+++ src/baekjoon/BaekJoon15894.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BaekJoon15894 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long input = sc.nextInt();
+		System.out.println((long) input * 4);
+	}
+}
---
+++ src/baekjoon/BaekJoon15962.java
@@ -0,0 +1,7 @@
+package baekjoon;
+
+public class BaekJoon15962 {
+	public static void main(String[] args) {
+		System.out.println("파이팅!!");
+	}
+}
